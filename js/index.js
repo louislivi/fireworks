e0664e7c3f9a08d5a6f20d4205af470f56f39c55
trhdwmz <90231030+trhdwmz@users.noreply.github.com>
2021-09-07 11:01:18 +0800

    Create index.js
    八年级看来

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||某|某|祝|你|生|日|快|乐|#rectangle|');
+      S.UI.simulate('|#countdown 3||老|师|祝|你|节|日|快|乐|#rectangle|');
     }
 
     S.Drawing.loop(function () {
